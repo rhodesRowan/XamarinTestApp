1151887ea2121a42fb9138957f15b9c072629d8c
Rowan Rhodes <rowandrhodes@rowans-mbp-2.home>
2021-01-07 22:35:36 +0000

    Update Home View Model

--- TestApp/ViewModels/HomePageViewModel.cs
+++ TestApp/ViewModels/HomePageViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using TestApp.Models;
+using TestApp.Services;
+
 namespace TestApp.ViewModels
 {
     public class HomePageViewModel
     {
+        private readonly DataManager dataManager = new DataManager();
+        ObservableCollection<WebLink> webLinks   = new ObservableCollection<WebLink>();
+        public ObservableCollection<WebLink> WebLinks { get { return webLinks; } }
+        
+
         public HomePageViewModel()
         {
+            addItems();
+        }
 
+        private void addItems()
+        {
+            var items = dataManager.getLocalWebLinks();
+
+            foreach(WebLink item in items)
+            {
+                webLinks.Add(item);
+            }
         }
     }
 }
